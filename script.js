8a99a8c7c91efee6b4e326ba9f7351e5d7be1781
sharvari0428 <sharvaridhende06@gmail.com>
2026-06-27 17:18:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,14 @@
-<script>
-window.onload = function () {
+document.querySelectorAll(".card button")
+.forEach(button => {
+  button.addEventListener("click", () => {
+    window.open(
+      "https://wa.me/919876543210?text=I want to book a puja.",
+      "_blank"
+    );
+  });
+});
 
-  const buttons = document.getElementsByTagName("button");
-
-  for (let i = 0; i < buttons.length; i++) {
-
-    if (buttons[i].innerText.includes("Check Availability")) {
-
-      buttons[i].onclick = function () {
-
-        const card = this.closest(".pandit-card");
-
-        let name = "Pandit";
-        if (card && card.querySelector("h3")) {
-          name = card.querySelector("h3").innerText;
-        }
-
-        alert("🕉️ Checking availability for " + name +
-        "\n\n📅 We will confirm slots on WhatsApp soon!");
-      }
-    }
-  }
-
-  // ✅ IMAGE FIX (inside onload)
-
-  const images = [
-    "img/pooja1.jpg",
-    "img/pooja2.jpg",
-    "img/pooja3.jpg"
-  ];
-
-  let savedImage = localStorage.getItem("poojaImage");
-
-  if (!savedImage) {
-    savedImage = images[Math.floor(Math.random() * images.length)];
-    localStorage.setItem("poojaImage", savedImage);
-  }
-
-  const img = document.querySelector(".pooja-img");
-  if (img) {
-    img.src = savedImage;
-  }
-
-};
-</script>
-const poojas = [
-  {
-    name: "Satyanarayan Puja",
-    img: "img/satyanarayan.jpg"
-  },
-  {
-    name: "Ganesh Puja",
-    img: "img/ganesh.jpg"
-  },
-  {
-    name: "Navgrah Puja",
-    img: "img/navgrah.jpg"
-  }
-];
-
-document.querySelectorAll(".pandit-card").forEach((card, i) => {
-  const img = card.querySelector("img");
-  if (img && poojas[i]) {
-    img.src = poojas[i].img;
-  }
+document.getElementById("callBtn")
+.addEventListener("click", () => {
+  window.location.href = "tel:+919876543210";
 });
